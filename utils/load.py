c3b158a2626c8b46e93df4405aba2ac5a20ca3bd
icolbert <ian.colbert711@gmail.com>
2018-11-01 12:36:23 -0700

    Integrated new DataLoader() and fixed the generator function

--- utils/load.py
+++ utils/load.py
@@ -7,9 +7,65 @@ import matplotlib.pyplot as plt
 
 from IPython.display import display, HTML
 
-classes = ["airplane","angel","basket","bear","belt","candle","crown","cat","dog","fish"]
+classes = ["airplane","angel","basket","bear","belt","candle","crown","cat"]
 
-class data_loader:
+class DataLoader:
+    '''
+    Data Loader for google AI challenge used for generating batches of data from training source with minimal delay
+    '''
+    def __init__(self,access_files,batch_size=16):
+        '''
+        Initialize a base data loader to load samples from a given access file
+        inputs:
+        -------
+        access_file: location of source folder
+        self: pointer to current class instance
+        batch_size:size of batch to be taken from each file in the access_files set
+        '''
+        assert isinstance(access_files,map),"Undesired input to the function, map iterable not provided to iterate through the files of the data"
+        assert isinstance(batch_size,int),"Undesired input to the function, batch size must be an integer"
+        self.iterators = []#Create an empty list of generators to access data from multiple classes
+        
+        for index,file in enumerate(access_files):
+            assert isinstance(file,str),"Undesired input, file name must be a string"
+            data_generator = pd.read_csv(file,usecols=["drawing","word"],chunksize=batch_size)
+            self.iterators.append(data_generator)
+        
+    def __next__(self):
+        '''
+        Proceed to the next batch of data
+        
+        inputs:
+        ------
+        self: pointer to class instance
+        
+        outputs:
+        -------
+        dataframe: (dtype:pandas dataframe)Dataframe of samples form different classes
+        '''
+        data = []
+        for it in self.iterators:
+            data.append(next(it));
+        dataframe = pd.concat(data)
+        dataframe = dataframe.sample(frac=1).reset_index(drop=True)
+        return dataframe
+    
+    def __iter__(self):
+        '''
+        Iterator to allow for iteration along dataset
+        
+        inputs:
+        -------
+        self:pointer to the instance of the class
+        
+        outputs:
+        -------
+        self:pointer to the instance of the class
+        '''
+        return self
+            
+
+class data_loader_v0:
     def __init__(self,access_files):
         '''
         Initialize a base data loader to load samples from a given access file
--- utils/vis.py
+++ utils/vis.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 from glob import glob
 from PIL import Image
 
-from .load import data_loader
+from .load import DataLoader, classes
 
 def sketchplot(data,row=None):
-    assert isinstance(data, (data_loader, ImageGenerator)), "data needs to be of type data_loader"
+    assert isinstance(data, (DataLoader, ImageGenerator)), "data needs to be of type data_loader"
     if row is None:
         row = np.random.randint(0,data.shape[0])
     points = data[row]['drawing']
@@ -24,52 +24,39 @@ def sketchplot(data,row=None):
     plt.title(data[row]['word'])
     plt.axis('off')
 
-class ImageGenerator(data_loader, keras.utils.Sequence):
+class ImageGenerator():
     imsize = (450, 450)
-    def __init__(self, access_files, **kwargs):
-        data_loader.__init__(self, access_files)
-        self.dim = kwargs.pop('dim', self.imsize)
-        self.batch_size = kwargs.pop('batch_size', 128)
+    def __init__(self, access_files, batch_size=16, **kwargs):
+        self.loader     = DataLoader(access_files, batch_size=batch_size)
+        self.dim        = kwargs.pop('dim', self.imsize)
+        self.batchSize  = batch_size*8
         self.n_channels = kwargs.pop('n_channels', 4)
-        self.n_classes  = self.labels.shape[-1]
-        self.indexes    = np.arange(self.data.shape[0])
-        self.shuffle    = kwargs.pop('shuffle', True)
-        self.on_epoch_end()
+        self.n_classes  = len(classes)
+        #self.indexes    = np.arange(self.data.shape[0])
+        #self.shuffle    = kwargs.pop('shuffle', True)
 
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(self.shape[0] / self.batch_size))
-    
-    def __getitem__(self, index):
-        '''
-        Returns images using the timestamped vectors in the data
-        '''
-        idxs = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+    def generateImages(self):
+        while True:
+            X = np.empty((self.batchSize, *self.dim, self.n_channels))
+            y = np.empty((self.batchSize,), dtype=int)
 
-        X, y = self.__generate_data(idxs)
+            df = next(self.loader)
+            for i, idx in enumerate(range(df.shape[0])):
+                (img, lbl) = self.vec2img(df, idx)
+                X[i,] = np.asarray(img)
+                y[i,] = lbl
 
-        return X, y
+            yield X, self.onehot(y)
 
-    def __generate_data(self, idxs):
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size, self.n_classes), dtype=int)
+    def onehot(self, y):
+        b = np.zeros((y.shape[0], self.n_classes))
+        b[np.arange(y.shape[0]), y] = 1
+        return b
 
-        for i, idx in enumerate(idxs):
-            (img, wrd) = self.vec2img(idx)
-            X[i,] = np.asarray(img)
-            y[i,] = wrd.values
-
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle:
-            self.data = self.data.sample(frac=1).reset_index(drop=True)
-
-    def vec2img(self,row=None):
+    def vec2img(self, df, row=None):
         if row is None:
-            row = np.random.randint(0,self.data.shape[0])
-        points = self.data.iloc[row]['drawing']
+            row = np.random.randint(0,df.shape[0])
+        points = df.iloc[row]['drawing']
         points = ast.literal_eval(points)
         plt.close()
         figure = plt.figure()
@@ -77,7 +64,7 @@ class ImageGenerator(data_loader, keras.utils.Sequence):
             [x,y] = xy
             plt.plot(x, y, marker='.')
         plt.axis('off')
-        return fig2img(figure, size=self.imsize), self.labels.iloc[row]
+        return fig2img(figure, size=self.imsize), classes.index(df.iloc[row]['word'])
 
 def fig2data ( fig ):
     """
@@ -121,61 +108,3 @@ def fig2img ( fig, size=None ):
         return background
     else:
         return image
-
-
-'''
-Use this as a template
-'''
-
-class DataGenerator(keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
-
-            # Store class
-            y[i] = self.labels[ID]
-
-        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
